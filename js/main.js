60e376b1e5fdf8bbefa42911d14018a71322a583
kasprzyk <piotkas1@gmail.com>
2017-06-21 17:08:59 +0200

    aktywne menu, jquery

--- js/main.js
+++ js/main.js
@@ -3,20 +3,10 @@
  */
 /*slider*/
 
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("slider");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-}
+$(document).ready(function(){
+    $('ul li a').click(function(){
+        $('ul li').removeClass("aktywnemenu");
+        $(this).parent().addClass("aktywnemenu");
+    });
+});

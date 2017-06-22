7c8dd6ba4991004974e85db4df2c5ab242c2bca4
jeziorskilukasz <l.jeziorski@britishclass.pl>
2017-06-22 15:25:30 +0200

    smooth_Scroll_JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+$(window).scroll(function () {
+    var hands2=$(".hands2")
+    var offset=$(this).scrollTop();
+    hands2.css("background-position-y",offset/2);
+    var hands=$(".hands")
+    hands.css("background-position-y",offset/1.8);
+// })
+// $(window).scroll(function () {
+//     var offset=$(this).scrollTop()
+    var hands1=$(".hands1");
+    hands1.css("background-position-y", offset/1.8);
+});
+
+
 // SMOOTH//
 $(document).ready(function () {
     $('a').click(function(){
@@ -7,7 +21,6 @@ $(document).ready(function () {
         return false;
     });
 });
-
 
 
 $( "#przekierowanie-zainteresowania" ).click(function() {

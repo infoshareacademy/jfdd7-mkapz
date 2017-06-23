c18599f3e2768c4a5bc36435bad0cd04588899ef
MartynaBartniczak <aszkee@gmail.com>
2017-06-23 16:38:44 +0200

    rwd popr

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
+
+//PARALAKSA W ZAJAWCE//
 $(window).scroll(function () {
     var hands2=$(".hands2")
     var offset=$(this).scrollTop();
     hands2.css("background-position-y",offset/2);
-    var hands=$(".hands")
-    hands.css("background-position-y",offset/1.8);
-// })
-// $(window).scroll(function () {
-//     var offset=$(this).scrollTop()
     var hands1=$(".hands1");
     hands1.css("background-position-y", offset/1.8);
 });

184673a1201754a202434b11e1779c601351a505
jeziorskilukasz <l.jeziorski@britishclass.pl>
2017-06-26 16:28:26 +0200

    smooth_Scroll_JS section function animate

--- js/main.js
+++ js/main.js
@@ -12,17 +12,67 @@ $(window).scroll(function () {
 });
 
 
-//SMOOTH-SCROLL//
+//SMOOTH-SCROLL-BEGIN//
 $(document).ready(function () {
-    $('a').click(function(){
+    $('a').click(function () {
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top-80
-        }, 800, function () {
-            $("#opisdlugi .fa-map-marker").addClass("rotate");
-        });
+            scrollTop: $($(this).attr('href')).offset().top - 80
+        }, 600);
         return false;
     });
 });
+//SMOOTH-SCROLL-END//
 
-//ANIMATION//
 
+//FUNCTION_ANIMATION-BEGIN//
+$(document).ready(function () {
+    $('#animate_1').click(function(){
+        $("#animate_1_on").addClass('rotatedown');
+    });
+});
+
+$(document).ready(function () {
+    $('#animate_2').click(function(){
+        $("#animate_2_on").addClass('rotatedown');
+    });
+});
+
+$(document).ready(function () {
+    $('#animate_3').click(function(){
+        $("#animate_3_on").addClass('rotatedown');
+    });
+});
+
+$(document).ready(function () {
+    $('#animate_4').click(function(){
+        $("#animate_4_on").addClass('rotatedown');
+    });
+});
+//FUNCTION_ANIMATION_END//
+
+
+//SCROLL_FUNCTIONS_BEGIN//
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.showconent').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},500);
+
+            }
+
+        });
+
+    });
+
+});
+//SCROLL_FUNCTIONS_END//

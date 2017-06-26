0e7ad545d04098ef71c3eb5ece925c7ff379a985
jeziorskilukasz <l.jeziorski@britishclass.pl>
2017-06-26 16:50:22 +0200

    smooth_Scroll_JS section function animate added div

--- js/main.js
+++ js/main.js
@@ -52,27 +52,21 @@ $(document).ready(function () {
 
 
 //SCROLL_FUNCTIONS_BEGIN//
-$(document).ready(function() {
 
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+/* Every time the window is scrolled ... */
+$(window).scroll( function(){
+    $('.showcontent').each( function(i){
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > bottom_of_object ){
 
-        /* Check the location of each desired element */
-        $('.showconent').each( function(i){
+            $(this).animate({'opacity':'1'},800);
 
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-
-            }
-
-        });
+        }
 
     });
 
 });
+
 //SCROLL_FUNCTIONS_END//
+
